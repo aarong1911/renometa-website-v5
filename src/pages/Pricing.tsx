cd9b97ca2a42e9c000920d0703103cdb79a98df9
Aaron G <Aarong1911@gmail.com>
2025-05-09 02:10:25 -0400

    Update Pricing.tsx

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -184,7 +184,7 @@ const Pricing = () => {
   ];
 
   return (
-    <ServicePageTemplate 
+    <PricingPageTemplate 
       {...pricingData}
       hideHero
       customContent={renderPricingContent()}
