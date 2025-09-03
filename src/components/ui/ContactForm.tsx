987d1b468da16d8457c1638447cd2e9c2331afa2
Aaron G <Aarong1911@gmail.com>
2025-09-03 01:39:11 -0400

    Update ContactForm.tsx

--- src/components/ui/ContactForm.tsx
+++ src/components/ui/ContactForm.tsx
@@ -141,7 +141,7 @@ const ContactForm = ({ onSuccess }: ContactFormProps) => {
           >
             I Consent to Receive SMS Notifications, Alerts & Occasional Marketing
             Communication from RenoMeta. Message frequency varies. Message & data
-            rates may apply. Text HELP to +1(954) 871-8466 for assistance. You can
+            rates may apply. Text HELP to +1-888-792-1166 for assistance. You can
             reply STOP to unsubscribe at any time.
           </label>
         </div>
